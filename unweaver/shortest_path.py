31220dceafa5c4718c8104dd2d07ce22b12e3c1e
Nick Bolten <nbolten@gmail.com>
2019-03-22 22:13:51 -0700

    Added distinct error message for invalid waypoints

--- unweaver/shortest_path.py
+++ unweaver/shortest_path.py
@@ -3,7 +3,7 @@ import networkx as nx
 from networkx.algorithms.shortest_paths import multi_source_dijkstra
 
 
-from .graph import prepare_search
+from .graph import waypoint_candidates
 from .exceptions import InvalidWaypoint
 
 
@@ -11,26 +11,40 @@ class NoPathError(Exception):
     pass
 
 
-# TODO: use origin/destination terminology and params, make them 2-tuples
-def shortest_path(
-    G, lon1, lat1, lon2, lat2, cost_function, invert=None, flip=None, edge_filter=None
-):
+def waypoint_legs(G, waypoints, cost_function, invert=None, flip=None):
+    pairs = zip(waypoints, waypoints[1:])
+    legs = []
+    for wp1, wp2 in pairs:
+        wp1_candidates = waypoint_candidates(
+            G, wp1[0], wp1[1], n=4, invert=invert, flip=flip
+        )
+        wp2_candidates = waypoint_candidates(
+            G, wp2[0], wp2[1], n=4, invert=invert, flip=flip, is_destination=True
+        )
+
+        # If closest points on the graph are on edges, multiple shortest path searches
+        # will be done (this is a good point for optimization in future releases) and the
+        # cheapest one will be kept.
+        # TODO: generalize to multi-waypoints.
+        graph_wp1 = _choose_candidate(wp1_candidates, cost_function)
+        graph_wp2 = _choose_candidate(wp2_candidates, cost_function)
+
+        legs.append([graph_wp1, graph_wp2])
+
+    return legs
+
+
+def route_legs(G, legs, cost_function, invert=None, flip=None, edge_filter=None):
     """Find the on-graph shortest path between two geolocated points.
 
     :param G: The routing graph.
     :type G: entwiner.DiGraphDB
-    :param lon1: Longitude of origin point.
-    :type lon1: float
-    :param lat1: Latitude of origin point.
-    :type lat1: float
-    :param lon2: Longitude of destination point.
-    :type lon2: float
-    :param lat2: Latitude of destination point.
-    :type lat2: float
+    :param legs: A list of origin-destination pairs as prepared by _choose_candidate.
+    :type legs: list
     :param cost_function: A networkx-compatible cost function. Takes u, v, ddict as
                      parameters and returns a single number.
     :type cost_function: callable
-    :param invert: A list of keys to "invert", i.e. multiply by 1, for any temporary
+    :param invert: A list of keys to "invert", i.e. multiply by -1, for any temporary
                    reversed edges - i.e. when finding routes half way along an edge.
     :type invert: list of str
     :param flip: A list of keys fo "flip", i.e. swap truthiness, for the same
@@ -44,35 +58,14 @@ def shortest_path(
     """
     # TODO: Extract invertible/flippable edge attributes into the profile.
     # NOTE: Written this way to anticipate multi-waypoint routing
-    waypoints = [(lon1, lat1), (lon2, lat2)]
-    pairs = zip(waypoints, waypoints[1:])
-    legs = []
-    for wp1, wp2 in pairs:
-        wp1_candidates = prepare_search(
-            G, wp1[0], wp1[1], n=4, invert=invert, flip=flip
-        )
-        wp2_candidates = prepare_search(
-            G, wp2[0], wp2[1], n=4, invert=invert, flip=flip, is_destination=True
-        )
-
-        # If closest points on the graph are on edges, multiple shortest path searches
-        # will be done (this is a good point for optimization in future releases) and the
-        # cheapest one will be kept.
-        # TODO: generalize to multi-waypoints.
-        seed_cluster_1 = _waypoint_seeds(wp1_candidates, cost_function)
-        seed_cluster_2 = _waypoint_seeds(wp2_candidates, cost_function)
-        for cluster in (seed_cluster_1, seed_cluster_2):
-            if cluster is None:
-                # FIXME: Should produce more specific feedback - which waypoint?
-                raise InvalidWaypoint(
-                    "One or more waypoint had no valid on-graph start points"
-                )
+    route_legs = []
+    for wp1, wp2 in legs:
         routes = []
-        for to_node, to_seed in seed_cluster_2.items():
+        for to_node, to_seed in wp2.items():
             try:
                 cost, path = multi_source_dijkstra(
                     G,
-                    sources=[k for k, v in seed_cluster_1.items()],
+                    sources=[k for k, v in wp1.items()],
                     target=to_node,
                     weight=cost_function,
                 )
@@ -83,7 +76,7 @@ def shortest_path(
                 continue
 
             from_node = path[0]
-            from_seed = seed_cluster_1[from_node]
+            from_seed = wp1[from_node]
             cost += from_seed["seed_cost"]
             cost += to_seed["seed_cost"]
 
@@ -102,13 +95,23 @@ def shortest_path(
             raise NoPathError("No viable path found.")
 
         best_cost, best_path, best_edges = sorted(routes, key=lambda x: x[0])[0]
-        legs.append((best_cost, best_path, best_edges))
+        route_legs.append((best_cost, best_path, best_edges))
 
     # TODO: Return multiple legs once multiple waypoints supported
-    return legs[0]
+    return route_legs[0]
 
 
-def _waypoint_seeds(candidates, cost_function, edge_filter=None):
+def _choose_candidate(candidates, cost_function, edge_filter=None):
+    """
+
+    :param candidates: Iterable of candidates generated by waypoint_candidates.
+    :type candidates: generator
+    :param cost_function: Callable compatible with networkx cost functions
+    :type cost_function: callable
+    :param edge_filter: A function that return True for valid edges, False for invalid.
+    :type edge_filter: callable
+
+    """
     if edge_filter is None:
         edge_filter = lambda x: True
 
